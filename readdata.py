0c9c6b2c49b8ec66a9b79eb5f357560abef4a270
MatsudaTakashi <mazda.takasky@gmail.com>
2016-04-30 16:27:21 +0900

    added readHIdata, and passed the test

--- readdata.py
+++ readdata.py
@@ -1,26 +1,54 @@
 #!/Users/takashi/.pyenv/shims/python
 
+import numpy as np
+import math
+
 
 def readindex(filename):
     """
-    return the dictionary
+    return the dictionary such as
+    {strain:index}
+    filename: the name of the strain list file
     """
     f_dic = open(filename)
     strain_index = dict()
     for cnt, line in enumerate(f_dic):
         if cnt == 0:
             continue
-        line.split(',')[0]
         strain_index.update({line.split(',')[0]: cnt-1})
 
     return strain_index
 
 
-def readHIdata(filename):
+def readHIdata(filename, strainindex):
     """
-    not yet implemented
+    return ndarray whose elements are tuples as follows
+    (StrainID, SerumID, HIvalue)
+    the type is
+    (uint4, uint4, f4)
     """
-    return 0
+
+    lenfhi = sum(1 for line in open(filename))
+    f_hi = open(filename)
+    mold_hiarray = []
+    for cnt, line in enumerate(f_hi):
+        if cnt == 0:
+            continue
+
+        parse = line.split(',')
+        # TODO: the entry that its value is out of range also should be
+        # into calculation
+        if parse[6][0] == '<':
+            continue
+        elif parse[1] not in strainindex or parse[4] not in strainindex:
+            continue
+        else:
+            mold_hiarray.append((strainindex[parse[1]],
+                                 strainindex[parse[4]],
+                                 math.log(float(parse[6]), 2)))
+
+    print mold_hiarray
+    return np.array(mold_hiarray)
 
 
 def test_readindex():
@@ -30,6 +58,7 @@ def test_readindex():
 
 
 def test_readHIdata():
-    filename = "../H3N2_HIdata/H3N2_integrated_/H3N2_HI_data.tsv"
-    res = readHIdata(filename)
-    assert res[0][0] == 5120
+    filename = "../H3N2_HIdata/H3N2_integrated_/H3N2_HI_data.csv"
+    strainindex = readindex("../H3N2_HIdata/H3N2_integrated_/H3N2_seq_data.csv")
+    res = readHIdata(filename, strainindex)
+    assert (strainindex['A/Wisconsin/67/2005'], strainindex['A/Wisconsin/67/2005'], math.log(5120, 2)) in res
