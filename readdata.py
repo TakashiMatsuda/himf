5a44cbc6668cb6798b941d0ae2d40a48c66e3daa
MatsudaTakashi <mazda.takasky@gmail.com>
2016-04-30 15:12:20 +0900

    initial commit

---
+++ readdata.py
@@ -0,0 +1,33 @@
+#!/Users/takashi/.pyenv/shims/python
+
+
+def readindex(filename):
+    """
+    return the dictionary
+    """
+    f_dic = open(filename)
+    strain_index = dict()
+    for cnt, line in enumerate(f_dic):
+        line.split(',')[0]
+        strain_index.update({line.split(',')[0]: cnt})
+
+    return strain_index
+
+
+def readHIdata(filename):
+    """
+    not yet implemented
+    """
+    return 0
+
+
+def test_readindex():
+    filename = "./H3N2_HIdata/H3N2_integrated_/H3N2_seq_data.csv"
+    res = readindex(filename)
+    assert res[0] == 'A/Akita/4/1993'
+
+
+def test_readHIdata():
+    filename = "./H3N2_HIdata/H3N2_integrated_/H3N2_HI_data.tsv"
+    res = readHIdata(filename)
+    assert res[0][0] == 5120
---
+++ splitdata.py
@@ -0,0 +1,16 @@
+#!/Users/takashi/.pyenv/shims/python
+
+
+def makedataset(ratio1, ratio2, ratio3, data):
+    """
+        Split the dataset to train, validate,
+        and test the calculation.
+        ratio1: ratio for training dataset
+        ratio2: ratio for validating dataset
+        ratio3: ratio for testing dataset
+        data: Numpy array(Matrix)
+    """
+
+
+def test_makedataset():
+    
