f2d5248747979cd2471f50b1c7421d3ac4c9e487
MatsudaTakashi <mazda.takasky@gmail.com>
2016-05-08 18:19:55 +0900

    to change the himf, serumid

--- himf.py
+++ himf.py
@@ -2,7 +2,7 @@
 
 """
     Calculate the MF for HI data
-    powered by tutorial.py in PyRSVD
+    powered by PyRSVD
     Author : Takashi Matsuda 2016
     These codes are under MIT License.
 """
@@ -18,7 +18,6 @@ def himf(LATENTDIM, REG):
     strainindex = readdata.readindex(fn_index)
     ratings = readdata.readHIdata(fn_hi, strainindex)
     print ratings
-
     # make sure that the ratings a properly shuffled
     np.random.shuffle(ratings)
 
@@ -32,8 +31,9 @@ def himf(LATENTDIM, REG):
     train = train[:v]
 
     from rsvd import RSVD
-    dims = (len(strainindex), len(strainindex))
-
+#    dims = (len(strainindex), len(strainindex))
+    print len(strainindex)
+    dims = (402, 214)
     model = RSVD.train(LATENTDIM, train, dims, probeArray=val, learnRate=0.0005, regularization=REG)
 
     sqerr = 0.0
@@ -44,16 +44,16 @@ def himf(LATENTDIM, REG):
         reslist.append([rating, model(strainID, serumID)])
         sqerr += err * err
     sqerr /= test.shape[0]
-    print np.array(reslist)
-    np.save('bestparam-res.npy', np.array(reslist))
-    """
-    f = open('./experiment/rmse-ldim-{0}-reg-{1}'.format(LATENTDIM, REG), 'a')
+#    print np.array(reslist)
+#    np.save('bestparam-res.npy', np.array(reslist))
+
+    f = open('./experiment2/rmse-ldim-{0}-reg-{1}'.format(LATENTDIM, REG), 'a')
     f.write("Test RMSE: {0}\n".format(np.sqrt(sqerr)))
     f.close()
-    """
+
     print "Test RMSE: ", np.sqrt(sqerr)
 
 
 if __name__ == '__main__':
-    print 'sys.argv[1]: {0}'.format(sys.argv[1])
+#    print 'sys.argv[1]: {0}'.format(sys.argv[1])
     himf(int(sys.argv[1]), float(sys.argv[2]))
--- main.py
+++ main.py
@@ -9,8 +9,11 @@ import subprocess as sbp
 
 if __name__ == '__main__':
     p = Pool(3)
-    for latentdim in xrange(1, 40, 1):
-        for reg in xrange(1, 40, 1):
-            # TODO: make subprocess, so this area should move out of this file
-            # subprocess.call()
-            sbp.call(["python", "./himf.py", str(latentdim), str(reg/1000.0)])
+    for cnt in xrange(1, 50):
+        for latentdim in xrange(1, 50, 1):
+            for reg in xrange(1, 500, 1):
+                # TODO: make subprocess, so this area should move out of this file
+                # subprocess.call()
+
+    #                himf.himf(latentdim, reg/100.0)
+                    sbp.call(["python", "./himf.py", str(latentdim), str(reg/1000.0)])
