3a5aff1b9307d92fc53110787f243ca1a7133177
MatsudaTakashi <mazda.takasky@gmail.com>
2016-05-10 02:10:52 +0900

    wrote heatmap

--- plotting.py
+++ plotting.py
@@ -10,6 +10,13 @@ mfdata = np.load('avgary-3.npy')
 print(mfdata)
 
 fig, ax = plt.subplots()
+xl = [(str(x/1000.0) if x % 50 < 3 else '') for x in range(1, 300, 3)]
+print xl
 #cmap = sns.cubehelix_palette(as_cmap=True, light=.9)
-ax = sns.heatmap(mfdata, cmap='bwr', vmin=1.3, vmax=1.8)
-fig.savefig('param-rmse-3-rb.pdf')
+ax = sns.heatmap(mfdata, cmap='bwr', vmin=1.3, vmax=1.8,
+                 xticklabels=xl,
+                 yticklabels=range(1, 36, 3))
+plt.title('RMSE By Parameter', fontsize=20)
+plt.xlabel(u'\u03bb', fontsize=24)
+plt.ylabel("k", fontsize=24)
+fig.savefig('param-rmse-4-rb.pdf')
